a1d92380080385ad471c082164872f5f8a2c3c4f
SIDDARTH-P-P <sidhusidharth2711@gmail.com>
2023-10-18 23:39:17 +0530

    Change to search method

--- script/script.js
+++ script/script.js
@@ -1,20 +1,27 @@
-async function Data(){
+let scearch_element = document.getElementById("scearch_element")
+function clicked(){
+  async function Data(){
     let res = await fetch('https://dummyjson.com/products')
     let data = await res.json()
-    data.products.map(element => {
-        console.log(element);
-        let cart = document.getElementById("cart")
-        let ele = ` <div class="col-lg-4 pt-5">
-        <div class="card" style="width: 18rem;">
-            <img src="${element.images[2]}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${element.title}</h5>
-              <p class="card-text">${element.description}</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+    console.log(data);
+    let d = data.products.find(element => element.title == scearch_element.value)
+    if(!d){
+      cart.innerHTML="not fount"
+    }
+    else{
+      let cart = document.getElementById("cart")
+      let ele = ` <div class="col-lg-4 pt-5">
+      <div class="card" style="width: 18rem;">
+          <img src="${d.images[0]}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${d.title}</h5>
+            <p class="card-text">${d.description}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
-    </div>`
-      cart.innerHTML += ele
-    });
-}
+        </div>
+  </div>`
+    cart.innerHTML = ele
+    }
+  }
 Data();
+}
